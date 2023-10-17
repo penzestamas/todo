83fbe6143fa0894357bf63d1727442c0b0e24c22
penzestamas <github.rsldp@simplelogin.com>
2023-10-17 10:53:04 +0200

    simplify code

--- script.js
+++ script.js
@@ -2,41 +2,21 @@ let newItemContent = document.querySelector(".new-item-input");
 let newItemButton = document.querySelector(".new-item-button");
 let todoList = document.querySelector(".task-list");
 
-let todos = [];
-// let newItemIndex = 0;
-
 function generateItem(todo) {
   return `
     <li class="list-item">
       <label class="checkbox-label">${todo}
         <input type="checkbox">
         <span class="checkmark"></span>
-
-        <nav class="list-item-menu">
-          <i class="fa-solid fa-chevron-up"></i>
-          <i class="fa-solid fa-chevron-down"></i>
-          <i class="fa-regular fa-pen-to-square"></i>
-          <i class="fa-regular fa-trash-can"></i>
-        </nav>
       </label>
     </li>
   `;
 }
 
-function generateList() {
-  todoList.innerHTML = "";
-  for (todo of todos) {
-    console.log(todo)
-    todoList.innerHTML += todo;
-  }
-}
-
 function addItem() {
   let newTodo = newItemContent.value;
   if (newTodo.trim() !== "") {
-    todos.push(generateItem(newTodo));
-    // newItemIndex++;
-    generateList();
+    todoList.innerHTML += generateItem(newTodo);
     newItemContent.value = "";
   }
 }
